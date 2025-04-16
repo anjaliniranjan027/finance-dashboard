281244f586fb1eb0fc7318ab47ae562cc9730a3f
anjali niranjan <anjaliniranjan.official@gmail.com>
2025-04-16 16:07:37 +0530

    First commit

---
+++ client/src/expanded-theme.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+
+declare module "@mui/material/styles/createPalette" {
+  interface PaletteColor {
+    [key: number]: string;
+  }
+
+  interface Palette {
+    tertiary: PaletteColor;
+}
+}
